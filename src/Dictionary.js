4a2fb879f4947f384cd61250d62676293801026c
00Dre00 <darkpunisher.andrea@gmail.com>
2021-12-01 15:13:32 +0100

    added to Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Dictionary.css";
 
 export default function Dictionary(){
+let[keyword, setKeyword]= useState("");
+
+    function search(event){
+        event.preventDefault();
+        alert(`Searching for ${keyword}`);
+    }
+
+function handleKeywordChange(event){
+    console.log(event.target.value);
+}
+
     return(
     <div className="Dictionary">
-        <form>
-            <input type="search"/>
+        <form onSubmit={search}>
+            <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
         </form>
+        <btn btn-primary></btn>
     </div>);
 }
